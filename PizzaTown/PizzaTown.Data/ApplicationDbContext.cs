f6fbbf71fb98298de1ad03573ecee39b626ae140
Ati <ativassileva@gmail.com>
2023-05-15 21:27:45 +0300

    added app db context

---
+++ PizzaTown/PizzaTown.Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaTown.Data.Models;
+
+namespace PizzaTown.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Meal> Meals { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Role> Roles { get; set; } = null!;
+    }
+}
